911651c17f3af60c51e9adfd80f6e4a514fa088b
Ryosuke Akiyama <r@554.jp>
2023-05-23 14:33:40 +0900

    Add checking whether HTTP header has been wrote

--- wrapper.go
+++ wrapper.go
@@ -1,18 +1,26 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 type ResponseWriterWrapper struct {
 	http.ResponseWriter
-	Code int
-	Size int64
+	Code        int
+	Size        int64
+	wroteHeader bool
 }
+
+var _ http.ResponseWriter = (*ResponseWriterWrapper)(nil)
 
 func (w *ResponseWriterWrapper) Header() http.Header {
 	return w.ResponseWriter.Header()
 }
 
 func (w *ResponseWriterWrapper) Write(b []byte) (n int, err error) {
+	if !w.wroteHeader {
+		w.WriteHeader(http.StatusOK)
+	}
 	n, err = w.ResponseWriter.Write(b)
 	if err == nil {
 		w.Size += int64(n)
@@ -21,6 +29,9 @@ func (w *ResponseWriterWrapper) Write(b []byte) (n int, err error) {
 }
 
 func (w *ResponseWriterWrapper) WriteHeader(statusCode int) {
-	w.Code = statusCode
+	if !w.wroteHeader {
+		w.Code = statusCode
+		w.wroteHeader = true
+	}
 	w.ResponseWriter.WriteHeader(statusCode)
 }
